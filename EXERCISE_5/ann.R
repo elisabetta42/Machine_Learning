020d317a3ede90532258e266d3bf3c7789c23ac7
Christian Arentsen <dr.c.arentsen@gmail.com>
2017-05-04 13:50:03 +0200

    almost finished 1st exercise of ex 5

--- EXERCISE_5/ann.R
+++ EXERCISE_5/ann.R
@@ -4,21 +4,42 @@ require(RSNNS)
 source('C:/Users/Christian/Documents/GitHub/Machine_Learning/EXERCISE_5/load_dataset.R')
 
 # Define nn training set
-trainlabels <- dataset[1:4000,1]
-trainlevels <- levels(trainlabels)
-nntrainingClass <- matrix(nrow = length(trainlevels), ncol = 10, data = 0)
 
-for (i in 1:length(trainlevels)) {
+trainlabels <- as.factor(dataset[1:4000,1])
+trainingset <- dataset[1:4000,-1]
+trainlevels <- levels(trainlabels)
+nntrainingClass <- matrix(nrow = length(trainlabels), ncol = 10, data = 0)
+
+#Define test set
+test <- dataset[4001:8000,-1]
+testlabels <- dataset[4001:8000,1]
+
+#Prepare nn test set.
+for (i in 1:length(trainlabels)) {
   matchList <- match(trainlevels, toString(trainlabels[i]))
   matchList[is.na(matchList)] <- 0
   nntrainingClass[i,] <- matchList
 }
 trainingClass <- as.data.frame(nntrainingClass)
 
+# each entry represents a hidden layer, the value the number of neurons
+size = c(20, 20, 20)
 
+model <- mlp(x = trainingset, y = trainingClass, size = size, maxit = 600, learnFunc = "Std_Backpropagation", learnFuncParams = c(0.04, 0))
+plotIterativeError(model)
 
+predictions <- predict(model, newdata = test)
 
+#Using the "predict" function we have recieved "predictions"
+responselist <- matrix(nrow = length(predictions[,1]), ncol = 1, data = "Na")
+for(i in 1:nrow(predictions)) {
+  responselist[i,] <- toString( which(predictions[i,]==max(predictions[i,])) - 1 )
+}
+responselist <- data.frame(responselist)
+responselist[,1] <- as.factor(responselist[,1])
+# Calculating the accuracy
+agreement_rbf <- responselist[,1] == testlabels
+table(agreement_rbf)
+prop.table(table(agreement_rbf))
 
-test <- dataset[4001:8000,-1]
-testlabels <- dataset[4001:8000,1]
-set.seed(300)
+           
