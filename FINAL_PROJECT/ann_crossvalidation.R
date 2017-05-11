fb4165bdeac0d82f6540c7e3685b278975dd6d9a
Christian Arentsen <dr.c.arentsen@gmail.com>
2017-05-11 23:28:11 +0200

    Finished the cross validation code for pca included

--- FINAL_PROJECT/ann_crossvalidation.R
+++ FINAL_PROJECT/ann_crossvalidation.R
@@ -1,4 +1,4 @@
-# ANN 10-Fold Cross Validation
+#### ANN 10-Fold Cross Validation ####
 
 # Imports
 require(caret)
@@ -31,7 +31,7 @@ networkLearningFunc = "Std_Backpropagation"
 networkLearningFuncParam = c(0.045, 0)
 
 #
-## Person dependent no PCA
+#### Person dependent no PCA ####
 dependent.accuracy <- table(1:length(dependent_folds))
 dependent.time.ann <- table(1:length(dependent_folds))
 dependent.time.predict <- table(1:length(dependent_folds))
@@ -85,14 +85,67 @@ for (i in 1:length(dependent_folds)){
 
 
 #
-## Person dependent PCA
+#### Person dependent PCA ####
+pca.dependent.accuracy <- table(1:length(pca.dependent_folds))
+pca.dependent.time.pca <- table(1:length(pca.dependent_folds))
+pca.dependent.time.ann <- table(1:length(pca.dependent_folds))
+pca.dependent.time.predict <- table(1:length(pca.dependent_folds))
+
 for (i in 1:length(pca.dependent_folds)){
+  #
+  # Devide into training and test
+  training_label <- as.factor(dataset[-folds[[i]], 1])
+  levels <- levels(training_label)
+  start.time <- Sys.time()
+  temp.training_set <- prcomp((dataset[-folds[[i]], -1]), retx = TRUE, center = TRUE, scale. = TRUE)
+  training_set <- temp.training_set$x
+  test_label <- dataset[folds[[i]], 1]
+  test_set <- dataset[folds[[i]], -1]
+  temp.test_set <- prcomp(test_set, retx = TRUE, center = TRUE, scale. = TRUE)
+  test_set <- predict(temp.test_set, test_set)
+  pca.dependent.time.pca[i] <- difftime(Sys.time(), start.time, units = "secs")
   
+  #
+  # Define network training set
+  temp.trainingClass <- matrix(nrow = length(training_label), ncol = 10, data = 0)
+  
+  # Prepare nn test set
+  for (z in 1:length(training_label)) {
+    matchList <- match(levels, toString(training_label[z]))
+    matchList[is.na(matchList)] <- 0
+    temp.trainingClass[z,] <- matchList
+  }
+  trainingClass <- as.data.frame(temp.trainingClass)
+  
+  #
+  # Train neural network model
+  start.time <- Sys.time()
+  model <- mlp(x = training_set, y = trainingClass, size = networkSize, maxit = networkMaxEpochs, learnFunc = networkLearningFunc, learnFuncParams = networkLearningFuncParam)
+  pca.dependent.time.ann[i] <- difftime(Sys.time(), start.time, units = "secs")
+  
+  #
+  # Prediction
+  start.time <- Sys.time()
+  prediction <- predict(model, newdata = test_set)
+  pca.dependent.time.predict[i] <- difftime(Sys.time(), start.time, units = "secs")
+  
+  #
+  # Calculate the accuracy of the predictions 
+  responselist <- matrix(nrow = length(prediction[,1]), ncol = 1, data = "Na")
+  for(z in 1:nrow(prediction)) {
+    responselist[z, ] <- toString(which(prediction[z, ] == max(prediction[z, ])) - 1)
+  }
+  responselist <- data.frame(responselist)
+  responselist[,1] <- as.factor(responselist[,1])
+  # Calculate accuracy
+  agreement <- responselist[,1] == test_label
+  tt <- prop.table(table(agreement))
+  pca.dependent.accuracy[i] <- tt['TRUE']
 }
 
 
 #
-## Person independent no PCA
+#### Person independent no PCA ####
 independent.accuracy <- table(1:length(independent_folds))
 independent.time.ann <- table(1:length(independent_folds))
 independent.time.predict <- table(1:length(independent_folds))
@@ -146,7 +199,60 @@ for (i in 1:length(independent_folds)){
 
 
 #
-## Person independent PCA
+#### Person independent PCA ####
+pca.independent.accuracy <- table(1:length(pca.independent_folds))
+pca.independent.time.pca <- table(1:length(pca.independent_folds))
+pca.independent.time.ann <- table(1:length(pca.independent_folds))
+pca.independent.time.predict <- table(1:length(pca.independent_folds))
+
 for (i in 1:length(pca.independent_folds)){
+  #
+  # Devide into training and test
+  training_label <- as.factor(dataset.shuffled[-folds[[i]], 1])
+  levels <- levels(training_label)
+  start.time <- Sys.time()
+  temp.training_set <- prcomp((dataset.shuffled[-folds[[i]], -1]), retx = TRUE, center = TRUE, scale. = TRUE)
+  training_set <- temp.training_set$x
+  test_label <- dataset.shuffled[folds[[i]], 1]
+  test_set <- dataset.shuffled[folds[[i]], -1]
+  temp.test_set <- prcomp(test_set, retx = TRUE, center = TRUE, scale. = TRUE)
+  test_set <- predict(temp.test_set, test_set)
+  pca.dependent.time.pca[i] <- difftime(Sys.time(), start.time, units = "secs")
   
+  #
+  # Define network training set
+  temp.trainingClass <- matrix(nrow = length(training_label), ncol = 10, data = 0)
+  
+  # Prepare nn test set
+  for (z in 1:length(training_label)) {
+    matchList <- match(levels, toString(training_label[z]))
+    matchList[is.na(matchList)] <- 0
+    temp.trainingClass[z,] <- matchList
+  }
+  trainingClass <- as.data.frame(temp.trainingClass)
+  
+  #
+  # Train neural network model
+  start.time <- Sys.time()
+  model <- mlp(x = training_set, y = trainingClass, size = networkSize, maxit = networkMaxEpochs, learnFunc = networkLearningFunc, learnFuncParams = networkLearningFuncParam)
+  pca.dependent.time.ann[i] <- difftime(Sys.time(), start.time, units = "secs")
+  
+  #
+  # Prediction
+  start.time <- Sys.time()
+  prediction <- predict(model, newdata = test_set)
+  pca.dependent.time.predict[i] <- difftime(Sys.time(), start.time, units = "secs")
+  
+  #
+  # Calculate the accuracy of the predictions 
+  responselist <- matrix(nrow = length(prediction[,1]), ncol = 1, data = "Na")
+  for(z in 1:nrow(prediction)) {
+    responselist[z, ] <- toString(which(prediction[z, ] == max(prediction[z, ])) - 1)
+  }
+  responselist <- data.frame(responselist)
+  responselist[,1] <- as.factor(responselist[,1])
+  # Calculate accuracy
+  agreement <- responselist[,1] == test_label
+  tt <- prop.table(table(agreement))
+  pca.dependent.accuracy[i] <- tt['TRUE']
 }
